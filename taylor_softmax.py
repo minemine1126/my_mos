27ca053f29551b3bbdbecd2f7e428fd08c995675
semyung <13241294892@163.com>
2020-12-18 14:44:34 +0800

    modify taylor_softmax

--- taylor_softmax.py
+++ taylor_softmax.py
@@ -1,3 +1,5 @@
+import torch
+
 # class taylor_softmax(nn.Module):
 #     def __init__(self, dim):
 #         super(taylor_softmax, self).__init__():
@@ -17,15 +19,26 @@ def _get_softmax_dim(name, ndim, stacklevel):
     return ret
 
 
-def taylor_softmax(input, dim=None, _stacklevel=3, dtype=None):
+#def taylor_softmax(input, dim=None, _stacklevel=3, dtype=None):
+    # type: (Tensor, Optional[int], int, Optional[int]) -> Tensor
     # if not torch.jit.is_scripting():
     #     if type(input) is not Tensor and has_torch_function((input,)):
     #         return handle_torch_function(
     #             softmax, (input,), input, dim=dim, _stacklevel=_stacklevel, dtype=dtype)
+    # if dim is None:
+    #     dim = _get_softmax_dim('softmax', input.dim(), _stacklevel)
+    # if dtype is None:
+    #     ret = input.softmax(dim)
+    # else:
+    #     ret = input.softmax(dim, dtype=dtype)
+    # return ret
+
+
+def taylor_softmax(input, dim=None, _stacklevel=3, power=2):
+    # type: (Tensor, Optional[int], int, int) -> Tensor
     if dim is None:
         dim = _get_softmax_dim('softmax', input.dim(), _stacklevel)
-    if dtype is None:
-        ret = input.softmax(dim)
-    else:
-        ret = input.softmax(dim, dtype=dtype)
-    return ret
+    for i in range(power + 1):
+        ret += input.pow(i)
+    denom = input.sum(dim, True).clamp(1e-12).expand_as(input)
+    return input / denom
